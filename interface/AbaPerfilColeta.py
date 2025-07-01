58badb334a873681996df403cee75148be1c2295
cristianveggian@gmail.com <cristianveggian@gmail.com>
2025-06-30 21:04:36 -0300

    Inserido na interface função de adicionar classes

--- interface/AbaPerfilColeta.py
+++ interface/AbaPerfilColeta.py
@@ -5,7 +5,7 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
     QWidget, QVBoxLayout, QFormLayout, QLabel, QLineEdit,
     QTextEdit, QPushButton, QComboBox, QScrollArea, QWidget, QGridLayout,
-    QSpinBox, QDoubleSpinBox, QHBoxLayout
+    QSpinBox, QDoubleSpinBox, QHBoxLayout, QFrame
 )
 from mne.channels import get_builtin_montages
 from interface.components.ToastMessage import ToastMessage
@@ -19,6 +19,7 @@ class AbaPerfilColeta(QWidget):
         # Campos do perfil
         self.input_nome = QLineEdit()
         self.input_descricao = QTextEdit()
+        self.input_descricao.setFixedHeight(self.input_descricao.fontMetrics().height() * 3)
 
         self.combo_montagem = QComboBox()
         self.combo_montagem.addItems(get_builtin_montages())
@@ -63,6 +64,27 @@ class AbaPerfilColeta(QWidget):
         form_layout.addRow("Canais:", scroll_area)
         form_layout.addRow("Nº de ciclos:", self.input_runs)
 
+        # Classes
+
+        # Botão "+"
+        self.btn_add_class = QPushButton("+")
+        self.btn_add_class.setToolTip("Adicionar nova etapa ao pipeline")
+        self.btn_add_class.setFixedSize(20, 20)
+        self.btn_add_class.setStyleSheet("""
+            QPushButton {
+                color: white;
+                background-color: #28a745;
+                border: none;
+                font-weight: bold;
+                font-size: 14px;
+                border-radius: 5px;
+            }
+            QPushButton:hover {
+                background-color: #218838;
+            }
+        """)
+        self.btn_add_class.clicked.connect(self.add_class)
+
         # Campos compostos
         rest_layout = QHBoxLayout()
         rest_layout.addWidget(QLabel("Média:"))
@@ -77,6 +99,28 @@ class AbaPerfilColeta(QWidget):
         mi_layout.addWidget(QLabel("Desvio:"))
         mi_layout.addWidget(self.input_mi_std)
         form_layout.addRow("Tempo de imagética:", mi_layout)
+
+                # Área de classes
+        self.scroll_layout = QVBoxLayout()
+        self.class_widgets = []  # (QLineEdit, QSpinBox)
+
+        classes_container = QWidget()
+        classes_container.setLayout(self.scroll_layout)
+        classes_scroll = QScrollArea()
+        classes_scroll.setWidgetResizable(True)
+        classes_scroll.setWidget(classes_container)
+
+        # Linha "Classes:" com botão +
+        classes_row = QHBoxLayout()
+        classes_row.addWidget(QLabel("Classes:"))
+        classes_row.addWidget(self.btn_add_class)
+        classes_row.addStretch()
+
+        form_layout.addRow(classes_row)
+        form_layout.addRow(classes_scroll)
+
+        # Adiciona uma classe padrão
+        self.add_class()
 
         layout.addLayout(form_layout)
 
@@ -107,8 +151,8 @@ class AbaPerfilColeta(QWidget):
             self.canais_mapeados[ch_name] = combo
 
     def salvar_perfil(self):
-        nome = self.input_nome.text().strip()
-        if not nome:
+        nome_perfil = self.input_nome.text().strip()
+        if not nome_perfil:
             self.toast = ToastMessage(self, "O nome do perfil não pode estar vazio.")
             return
 
@@ -118,12 +162,22 @@ class AbaPerfilColeta(QWidget):
             if idx > 0:  # Ignorar "Ignorar"
                 canais[nome_canal] = int(combo.currentText())
 
+        classes = {}
+        for _, nome_input, valor_input in self.class_widgets:
+            nome_classe = nome_input.text().strip()
+            if nome_classe:
+                classes[nome_classe] = valor_input.value()
+        if not classes:
+            self.toast = ToastMessage(self, "Adicione pelo menos uma classe.")
+            return
+
         perfil = {
-            "nome": nome,
+            "nome": nome_perfil,
             "descricao": self.input_descricao.toPlainText(),
             "montagem": self.combo_montagem.currentText(),
             "canais": canais,
             "ciclos": self.input_runs.value(),
+            "classes": classes,
             "tempo_descanso": {
                 "mean": self.input_rest_mean.value(),
                 "std": self.input_rest_std.value()
@@ -134,7 +188,52 @@ class AbaPerfilColeta(QWidget):
             }
         }
 
-        with open(os.path.join('data','profiles', f"{nome}.json"), "w", encoding="utf-8") as f:
+        with open(os.path.join('data','profiles', f"{nome_perfil}.json"), "w", encoding="utf-8") as f:
             json.dump(perfil, f, indent=4)
 
-        self.toast = ToastMessage(self, f"Perfil '{nome}' salvo com sucesso.")
+        self.toast = ToastMessage(self, f"Perfil '{nome_perfil}' salvo com sucesso.")
+
+    def add_class(self):
+
+        # Container do bloco
+        container = QFrame()
+        layout_class = QHBoxLayout(container)
+
+        # botão de remover
+        botao_remover = QPushButton("-")
+        botao_remover.setFixedSize(20, 20)
+        botao_remover.setStyleSheet("""
+            QPushButton {
+                color: white;
+                background-color: #ff0000;
+                border: none;
+                font-weight: bold;
+                font-size: 14px;
+                border-radius: 5px;
+            }
+            QPushButton:hover {
+                background-color: #b30000;
+            }
+        """)
+        botao_remover.clicked.connect(lambda: self.rmv_class(container))
+
+        class_name = QLineEdit("Nome")
+        class_value = QSpinBox()
+
+        layout_class.addWidget(class_name)
+        layout_class.addWidget(class_value)
+
+        layout_class.addStretch()
+        layout_class.addWidget(botao_remover)
+
+        # Adiciona ao layout de scroll
+        self.scroll_layout.addWidget(container)
+        self.class_widgets.append((container, class_name, class_value))
+
+    def rmv_class(self, container):
+        for i, (frame, _, _) in enumerate(self.class_widgets):
+            if frame == container:
+                self.scroll_layout.removeWidget(frame)
+                frame.deleteLater()
+                self.class_widgets.pop(i)
+                break
